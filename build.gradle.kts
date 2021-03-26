5c489e99d311e9d1264f37b3616718df3174a3b8
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-03-26 13:03:33 +0100

    init repo

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("multiplatform") version "1.4.21"
+}
+
+group = "pl.mareklangiewicz"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    sourceSets {
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit5"))
+                implementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+                runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "dbus-kotlin"
+
