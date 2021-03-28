c5365c1e25dad57ab234b210571bf84da1109f93
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-03-28 21:52:00 +0200

    Use deprecated CreateInterface for code generation

--- dbus-kotlin/src/jvmTest/kotlin/DBusKotlinGenerator.kt
+++ dbus-kotlin/src/jvmTest/kotlin/DBusKotlinGenerator.kt
@@ -1,27 +1,42 @@
+import org.freedesktop.dbus.utils.bin.CreateInterface
 import org.freedesktop.dbus.utils.generator.InterfaceCodeGenerator
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
-import pl.mareklangiewicz.dbuskotlin.dbusMutterIntro
+import java.io.File
 import kotlin.test.Ignore
+import kotlin.test.fail
 
 
 // TODO later: move all generated interfaces to separate gradle modules (libs) like dbus-kotlin-gnome, etc..
 
 internal class DBusKotlinGenerator {
 
+    fun dbusCreateInterface(
+        busName: String,
+        objectPath: String = "/" + busName.replace('.', '/'),
+        systemBus: Boolean = false
+    ) {
+        val outputDir = File(objectPath.trimStart('/'))
+        outputDir.exists() || outputDir.mkdirs() || fail("Can not create $outputDir")
+        CreateInterface.main(arrayOf(if (systemBus) "--system" else "--session", "--create-files", busName, objectPath))
+        // TODO_later: implement own code generation in kotlin. Both CreateInterface and InterfaceCodeGenerator are broken
+    }
+
+    @Test
+    fun dbusCreateInterfaceMutterDisplayConfig() = dbusCreateInterface("org.gnome.Mutter.DisplayConfig")
+
     @Ignore
     @Test
-    fun dbusGenerateSomeMutterStuff() {
-        InterfaceCodeGenerator.main(arrayOf(
-            "org.gnome.Mutter.DisplayConfig",
-            "/org/gnome/Mutter/DisplayConfig",
+    fun dbusGenerateSomeMutterStuff() { // Broken: InterfaceCodeGenerator does not create correct tupples as return values
+        InterfaceCodeGenerator.main(
+            arrayOf(
+                "org.gnome.Mutter.DisplayConfig",
+                "/org/gnome/Mutter/DisplayConfig",
 //            "--system",
-            "--session",
+                "--session",
 //        "--enable-dtd-validation",
-            "--outputDir",
-            "../dbus-kotlin/src/jvmMain/java",
-        ))
-
+                "--outputDir",
+                "../dbus-kotlin/src/jvmMain/java",
+            )
+        )
     }
 }
