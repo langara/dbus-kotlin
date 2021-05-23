20059a82dbd7f99a7f41b1594b36d36ceba6c4e5
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-05-23 12:34:27 +0200

    update

--- buildSrc/src/main/kotlin/DBusKotlinKonf.kt
+++ buildSrc/src/main/kotlin/DBusKotlinKonf.kt
@@ -1,8 +1,6 @@
 object DBusKotlinKonf {
-    const val verInt = 8
+    const val verInt = 9
     const val verStr = "0.0.0$verInt"
-    const val name = "dbus-kotlin" // should always be kept the same as rootProject.name (settings.gradle.kts)
     const val group = "pl.mareklangiewicz"
-    const val author = "Marek Langiewicz"
     const val description = "D-Bus for Kotlin"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("pl.mareklangiewicz.deps.settings")
 }
 
-rootProject.name = "dbus-kotlin" // should always be kept the same as DBusKotlinKonf.name
+rootProject.name = "dbus-kotlin"
 
 include(":dbus-kotlin")
 include(":dbus-generated")
