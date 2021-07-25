67462b697e7400465c66bf3b1594247ef9442141
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-07-25 21:17:15 +0200

    update

--- dbus-kotlin/build.gradle.kts
+++ dbus-kotlin/build.gradle.kts
@@ -12,7 +12,7 @@ kotlin {
         compilations.all {
             kotlinOptions {
                 jvmTarget = "1.8"
-                useIR = true
+                freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
             }
         }
         testRuns["test"].executionTask.configure {
